94cb58695d29b87a920ddaadfa6c0951fbe3c11c
MinaGeo <20P4198@eng.asu.edu.eg>
2022-08-04 02:27:04 +0200

    This is Third Version.

--- src/main/kotlin/MainFile.kt
+++ src/main/kotlin/MainFile.kt
@@ -1,9 +1,9 @@
 fun main()
 {
-    //This is Second version
+    //This is Third version
 
-    var number1 = 25.46
-    var number2 = 77.24
+    var number1 = 483
+    var number2 = 600.5
 
     var result = number1+number2
     println(result)
